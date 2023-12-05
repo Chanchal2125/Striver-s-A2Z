936676c1a52e4b0f1227a026432895f261dca2d8
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2023-12-05 13:50:29 +0530

    Create Pattern 3.cpp

---
+++ Patterns/Pattern 3.cpp
@@ -0,0 +1,16 @@
+void nTriangle(int n) {
+	// Write your code here
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= i; j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+/*
+1
+12
+123
+1234    n = 4
+*/
