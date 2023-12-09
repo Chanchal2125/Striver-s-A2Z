8308c7578f6cd2730a21c285fd4f3c7fd00c91c1
Chanchal Gupta <61290731+Chanchal2125@users.noreply.github.com>
2023-12-09 19:48:56 +0530

    Create Pattern 16.cpp

---
+++ Patterns/Pattern 16.cpp
@@ -0,0 +1,17 @@
+void alphaRamp(int n) {
+    // Write your code here.
+    char z = 'A';
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<z<<" ";
+        }
+        cout<<endl;
+        z = char(z+1);
+    }
+}
+
+/*
+A
+BB
+CCC  n = 3
+*/
